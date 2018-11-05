987064ed9046f57fe830b4f038b91909a7d27fe8
Kendra Nixon <knixon000@nyc.yearup.org>
2018-11-05 11:15:29 -0500

    Words show up, do not replace

--- w03/d1/morning_exercise/main.js
+++ w03/d1/morning_exercise/main.js
@@ -1,1 +1,14 @@
 // Write a program that will `console.log` the numbers from 1 to 100. But for multiples of three `console.log` “Fizz” instead of the number and for the multiples of five `console.log` “Buzz”. For numbers which are multiples of both three and five `console.log` “FizzBuzz”.
+
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+  if (i%3 == 0) {
+    console.log("Fizz");
+  }
+  if (i%5 == 0) {
+    console.log("Buzz");
+  }
+  if (i%3 == 0 && i%5 == 0){
+    console.log("FizzBuzz");
+  }
+}
