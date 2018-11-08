e74711b940ecec8c2e4a34009c5cdd306d883e8c
Kendra Nixon <knixon000@nyc.yearup.org>
2018-11-08 16:08:54 -0500

    workshop requirements completed

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,18 @@
 //This comes from data.js
 var gifs = window.data.data;
 
+//function to select random gifs from data.js
+function randomGif(){
+  //target or select a gif
+  let gifElement = document.getElementById('gif');
+  //selects random gifs from the index of gifs
+  let randomGifs = Math.floor(Math.random()*gifs.length);
+  //manipulate the element to be random
+  gifElement.src = gifs[randomGifs].images.original.url;
+
+}
+
+
+//allows the button to cycle through the random gifs
+let button = document.querySelector('button');
+button.addEventListener('click', randomGif);
