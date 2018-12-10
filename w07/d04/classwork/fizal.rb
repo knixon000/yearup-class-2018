3d68dfbf8f6ce53fa2d2566f66829c8017fe5db4
Kendra Nixon <knixon000@nyc.yearup.org>
2018-12-10 13:07:25 -0500

    w07 d04 workshop

--- w07/d04/classwork/fizal.rb
+++ w07/d04/classwork/fizal.rb
@@ -1,11 +1,26 @@
 require 'sinatra'
+require 'geocoder'
+require 'forecast_io'
 
-get '/random' do
-  ## when someone asks for localhost:3000/random
+get '/weather' do
+  #get the latitude and longitude
+  lat_lon =  Geocoder.search("nyc").first.coordinates
+
+  # get a weather forecast
+  ForecastIO.api_key = 'a65700d3d69f77123ba1cbece8365328'
+  forecast = ForecastIO.forecast(lat_lon[0], lat_lon[1])
+  return forecast["currently"]["summary"]
+  @weather = forecast["currently"]["summary"]
   erb :index
 end
 
-get '/frank-says' do
-  #when someone asks for localhost:3000/frank-says
-  '<h1>Diddy</h1>'
-end
+#
+# get '/random' do
+#   ## when someone asks for localhost:3000/random
+#   erb :index
+# end
+#
+# get '/frank-says' do
+#   ##when someone asks for localhost:3000/frank-says
+#   '<h1>Diddy</h1>'
+# end
